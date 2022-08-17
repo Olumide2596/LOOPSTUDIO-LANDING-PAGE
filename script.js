bae3220fc9adb0d03c78eaf480e44b2d2aa0dc74
Olumide2596 <olumide2596@gmail.com>
2022-08-17 12:32:22 +0100

    feedback changes
    made changes according to the feedback recieved

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
 const navigation = document.querySelector('.navigation');
+const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', function () {
 	navigation.classList.remove('hidden');
+	navigation.insertAdjacentHTML(
+		'beforeend',
+		`<nav>
+		<ul>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Careers</a></li>
+			<li><a href="#">Events</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">Support</a></li>
+		</ul>
+	</nav>`
+	);
 });
 
 close.addEventListener('click', function () {
