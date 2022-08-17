f18bd58bd3f0ae3ad8555c3d1c3c888c2cf24cba
Olumide2596 <olumide2596@gmail.com>
2022-08-17 12:58:58 +0100

    fixed minor bug

--- script.js
+++ script.js
@@ -3,22 +3,10 @@
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
 const navigation = document.querySelector('.navigation');
-const nav = document.querySelector('nav');
+const nav = document.getElementsByName('nav');
 
 hamburger.addEventListener('click', function () {
 	navigation.classList.remove('hidden');
-	navigation.insertAdjacentHTML(
-		'beforeend',
-		`<nav>
-		<ul>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Careers</a></li>
-			<li><a href="#">Events</a></li>
-			<li><a href="#">Products</a></li>
-			<li><a href="#">Support</a></li>
-		</ul>
-	</nav>`
-	);
 });
 
 close.addEventListener('click', function () {
