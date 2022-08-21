7af50f16fd689b24981ab0d4344ee933eb6d69db
Olumide2596 <olumide2596@gmail.com>
2022-08-21 10:32:46 +0100

    practicing

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ hamburger.addEventListener('click', function () {
 close.addEventListener('click', function () {
 	navigation.classList.add('hidden');
 });
+
+const logo = document.querySelector('.logo');
+const creations = document.querySelector('.creations');
+
+logo.addEventListener('click', function (e) {
+	creations.scrollIntoView({ behavior: 'smooth' }); // new method
+
+	// const position = creations.getBoundingClientRect();
+	// window.scrollTo({
+	// 	left: position.left + window.pageXOffset,
+	// 	top: position.top + window.pageYOffset,
+	// 	behavior: 'smooth',
+	// });
+});
+
+// Math.floor(Math.random() * (max - min + 1) + min)
+
+// const header = document.querySelector('header');
+// const message = document.createElement('div');
+// message.innerHTML = `We use cookies to improve functionality and analytics <button class="btn btn--close--cookie" style= 'background-color:pink;margin-left: 20px; border-radius: 20px'>Got it</button>`;
+// header.insertAdjacentHTML(
+// 	'beforebegin',
+// 	`<div style='text-align:center; padding:10px; background-color:gray'>We use cookies to improve functionality and analytics <button class="btn btn--close--cookie" style= 'background-color:pink;margin-left: 20px; border-radius: 20px'>Got it</button></div>`
